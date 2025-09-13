d3f1afc0163607937d0d658f8b41bbf6a2a0f589
Long Trần Bảo <baolong242005@gmail.com>
2025-09-13 14:58:05 +0700

    Add thongtin and tinhtuoi functions

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-function showInfo(name, age) {
+function thongtin(name, age) {
   console.log("Tên:", name);
   console.log("Tuổi:", age);
 }
+thongtin("Chí", 22);
 
-showInfo("Chí", 22);
+function tinhtuoi(namsinh) {
+  const currentYear = new Date().getFullYear();
+  return currentYear - namsinh;
+}
+
+
+const year = 2003;
+const age = tinhtuoi(year);
+console.log(`Năm sinh: ${year} → Tuổi: ${age}`);
